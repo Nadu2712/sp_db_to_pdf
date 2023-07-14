304f8d2487d1558aefeb2ff317d5026ab4333447
Nadushanth Sathiskumar <snadushanth2000@gmail.com>
2023-07-14 10:09:53 +0530

    Initial commit

---
+++ src/main/java/com/example/sp_db_to_pdf/SpDbToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.sp_db_to_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpDbToPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpDbToPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sp_db_to_pdf/SpDbToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp_db_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpDbToPdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
